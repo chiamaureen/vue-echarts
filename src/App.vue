8cc23ebd44fe6708931ea50084220e04d7513618
Maureen <chiamaureenla@gmail.com>
2022-05-02 22:42:50 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-main>
+      <!-- <lineChart chartId="chart" :chartOptions="{}" width="500px" height="500px"/> -->
+      <Map />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+// import lineChart from './components/lineChart';
+import Map from './components/map';
+
+export default {
+  name: 'App',
+
+  components: {
+    // lineChart,
+    Map
+  },
+
+  data: function() {
+    return {
+        //
+      };
+  },
+};
+</script>
---
+++ src/components/lineChart.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div :id="chartId" :style="{ width: width, height: height }"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    chartId: {
+      type: String,
+      required: true
+    },
+    width: {
+      type: String,
+      default: "500px"
+    },
+    height: {
+      type: String,
+      default: "500px"
+    },
+    chartOptions: {
+      type: Object,
+      required: true
+    },
+  },
+  mounted() {
+    this.createChart()
+  },
+  methods: {
+    createChart() {
+      // 基於準備好的dom，初始化echarts例項
+      let chart = this.$echarts.init(document.getElementById(this.chartId));
+      // 指定圖表的配置項和資料
+      var option = {
+        title: {
+          text: "一週價格走勢"
+        },
+        tooltip: {},
+        legend: {
+          data: ["價格"]
+        },
+        xAxis: {
+          type: "category",
+          data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+        },
+        yAxis: {
+          type: "value"
+        },
+        series: [
+          {
+            name: "價格",
+            data: [220, 202, 231, 243, 225, 220, 213],
+            type: "line",
+            smooth: true
+          }
+        ]
+      }
+      chart.setOption(option);
+    }
+  }
+}
+</script>
